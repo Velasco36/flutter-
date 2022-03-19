d86ab16fa467b0346cc377fb2c15fb17ddc43de6
Miguel Velasco <migue16velasco@gmail.com>
2022-03-19 12:59:18 -0400

    portada de un login

--- lib/main.dart
+++ lib/main.dart
@@ -25,26 +25,77 @@ class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("DESARROLLAOR VELASCO"),
-      ),
-      body: Center(
-        child: RaisedButton(
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Icon(Icons.access_time),
-                SizedBox(
-                  width: 5,
-                ),
-                Text("Hora"),
-              ],
-            ),
-            onPressed: () {
-              var t = DateTime.now();
-              print(t);
-            }),
-      ),
+      body: cuerpo(),
     );
   }
+}
+
+Widget cuerpo() {
+  return Container(
+    decoration: BoxDecoration(
+        image: DecorationImage(
+            image: NetworkImage(
+                "https://www.solofondos.com/wp-content/uploads/2015/12/eb030ae837c1c43420b377e290cd4937-1024x640.jpg"),
+            fit: BoxFit.cover)),
+    child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          nombre(),
+          campoUsuario(),
+          campocontrasena(),
+          SizedBox(
+            height: 10,
+          ),
+          oppenbutton()
+        ],
+      ),
+    ),
+  );
+}
+
+Widget nombre() {
+  return Text(
+    "sig in",
+    style: TextStyle(
+        color: Colors.white, fontSize: 35.0, fontWeight: FontWeight.bold),
+  );
+}
+
+Widget campoUsuario() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+    child: TextField(
+      decoration: InputDecoration(
+        hintText: "User",
+        fillColor: Colors.white,
+        filled: true,
+      ),
+    ),
+  );
+}
+
+Widget campocontrasena() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+    child: TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: "Password",
+        fillColor: Colors.white,
+        filled: true,
+      ),
+    ),
+  );
+}
+
+Widget oppenbutton() {
+  return FlatButton(
+      color: Colors.blueAccent,
+      padding: EdgeInsets.symmetric(horizontal: 100, vertical: 10),
+      onPressed: () {},
+      child: Text(
+        " Enter",
+        style: TextStyle(fontSize: 25, color: Colors.white),
+      ));
 }
