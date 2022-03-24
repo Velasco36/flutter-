5d9216121a937819996c64012a611749472f0e05
Miguel Velasco <migue16velasco@gmail.com>
2022-03-24 14:19:45 -0400

    navegando a otra pagina mediante un boton

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/pagina02.dart';
 
 void main() => runApp(MiApp());
 
@@ -25,77 +26,25 @@ class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: cuerpo(),
+      appBar: AppBar(
+        title: Text("Pagina WEB"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Home',
+            ),
+            RaisedButton(
+                child: Text("Ir a la siguiente pagina"),
+                onPressed: () => {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: ((context) => pagina02())))
+                    })
+          ],
+        ),
+      ),
     );
   }
-}
-
-Widget cuerpo() {
-  return Container(
-    decoration: BoxDecoration(
-        image: DecorationImage(
-            image: NetworkImage(
-                "https://www.solofondos.com/wp-content/uploads/2015/12/eb030ae837c1c43420b377e290cd4937-1024x640.jpg"),
-            fit: BoxFit.cover)),
-    child: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          nombre(),
-          campoUsuario(),
-          campocontrasena(),
-          SizedBox(
-            height: 10,
-          ),
-          oppenbutton()
-        ],
-      ),
-    ),
-  );
-}
-
-Widget nombre() {
-  return Text(
-    "sig in",
-    style: TextStyle(
-        color: Colors.white, fontSize: 35.0, fontWeight: FontWeight.bold),
-  );
-}
-
-Widget campoUsuario() {
-  return Container(
-    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-    child: TextField(
-      decoration: InputDecoration(
-        hintText: "User",
-        fillColor: Colors.white,
-        filled: true,
-      ),
-    ),
-  );
-}
-
-Widget campocontrasena() {
-  return Container(
-    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-    child: TextField(
-      obscureText: true,
-      decoration: InputDecoration(
-        hintText: "Password",
-        fillColor: Colors.white,
-        filled: true,
-      ),
-    ),
-  );
-}
-
-Widget oppenbutton() {
-  return FlatButton(
-      color: Colors.blueAccent,
-      padding: EdgeInsets.symmetric(horizontal: 100, vertical: 10),
-      onPressed: () {},
-      child: Text(
-        " Enter",
-        style: TextStyle(fontSize: 25, color: Colors.white),
-      ));
 }
